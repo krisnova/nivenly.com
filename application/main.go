355a6a10ef830833691bba3274ee9ae2aabdb89b
Kris Nóva <kris@nivenly.com>
2021-05-14 16:23:17 -0700

    working boops interpolate bjorno land
    Signed-off-by: Kris Nóva <kris@nivenly.com>

---
+++ application/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"sync"
+
+	bjorno "github.com/kris-nova/bjorno"
+)
+
+func main() {
+	cfg := &bjorno.ServerConfig{
+		InterpolateExtensions: []string{
+			".html",
+		},
+		BindAddress:    ":1313",
+		ServeDirectory: "../public",
+		DefaultIndexFiles: []string{
+			"index.html",
+		},
+	}
+	bjorno.Runtime(cfg, &Nivenly{
+		RemoteAddress: "boops",
+	})
+}
+
+type Nivenly struct {
+	RemoteAddress string
+	mutex sync.Mutex
+}
+
+func (v *Nivenly) Values() interface{} {
+	return v
+}
+
+func (v *Nivenly) Refresh() {
+}
+
+func (v *Nivenly) Lock() {
+	v.mutex.Lock()
+}
+
+func (v *Nivenly) Unlock() {
+	v.mutex.Unlock()
+}
